9d9b335ef3ad3a44bc2a3616e749529380f1edb3
hunterherbst <hunter.d.herbst2k@gmail.com>
2019-09-04 23:27:44 -0400

    Initial Commit

---
+++ Config.java
@@ -0,0 +1,228 @@
+package resources;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Hashtable;
+
+/**
+ * This class is used to read configuration files and load data into the game.
+ * @author Hunter Herbst
+ *
+ */
+public class Config 
+{
+
+	private BufferedReader br;//BufferedReader used to read the file
+	private String filename;//The name of the config file
+	private Hashtable<String, ConfigAttribute> attributes;//A hashtable created from the attributes created in the config file
+	
+	/**
+	 * Open a new Configuration file with a given file name.
+	 * @param filename The file name of the desired Configuration file.
+	 */
+	public Config(String filename)
+	{
+		setFilename(filename);
+		attributes = new Hashtable<String, ConfigAttribute>();
+		File cfgDir = new File("src/configs/" + filename);
+		try
+		{
+			br = new BufferedReader(new FileReader(cfgDir));
+		}
+		catch(IOException ioe)
+		{
+			//TODO change the error output to run on a boolean value to change between output types
+			
+			//Use for stack trace debugging
+			//ioe.printStackTrace();
+			
+			//Use for user-friendly output
+			System.out.println("Error: Could not open config file \"" + filename + "\"");
+		}
+	}
+	
+	public Config(String filename, Boolean loadImmediately)
+	{
+		setFilename(filename);
+		attributes = new Hashtable<String, ConfigAttribute>();
+		File cfgDir = new File("src/configs/" + filename);
+		try
+		{
+			br = new BufferedReader(new FileReader(cfgDir));
+		}
+		catch(IOException ioe)
+		{
+			//TODO change the error output to run on a boolean value to change between output types
+			
+			//Use for stack trace debugging
+			//ioe.printStackTrace();
+			
+			//Use for user-friendly output
+			System.out.println("Error: Could not open config file \"" + filename + "\"");
+		}
+		if(loadImmediately)
+		{
+			loadConfig();
+		}
+	}
+	
+	
+	/*
+	 * Getter and setter methods for the file name
+	 */
+	
+	/**
+	 * Set the config file location
+	 * @param filename The name of the config file
+	 */
+	private void setFilename(String filename)
+	{
+		this.filename = filename;
+	}
+	
+	/**
+	 * Get the config file name
+	 * @return The name of the config file
+	 */
+	private String getFilename()
+	{
+		return this.filename;
+	}
+	
+	
+	/*
+	 * Methods for getting and using ConfigAttributes
+	 */
+	
+	public ConfigAttribute getAttribute(String key)
+	{
+		return attributes.get(key);
+	}
+	
+	public void loadConfig()
+	{
+		String configText = getText();
+		this.done();
+		//System.out.println(configText);
+		
+		String[] splitAttributes = configText.substring(0, configText.length()-1).split(";");
+	
+		for(int i = 0; i < splitAttributes.length; i++)
+		{
+			addAttribute(splitAttributes[i]);
+		}
+	}
+	
+	private void addAttribute(String attribute)
+	{
+		String attr = attribute.trim();
+		
+		//System.out.println(attr);
+		
+		String[] attrComps = attr.split("\\n");
+		
+		ConfigAttribute tmp = new ConfigAttribute(attrComps[0].trim().substring(1, attrComps[0].trim().length()-1));
+		//System.out.println(tmp.getName());
+		
+		for(int i = 2; i < attrComps.length-1; i++)
+		{
+			String[] component = new String[3];
+			
+			component[0] = attrComps[i].trim().substring(0,1);
+			component[1] = attrComps[i].trim().substring(2).split("=")[0];
+			component[2] = attrComps[i].trim().substring(2).split("=")[1];
+		
+		
+			switch(component[0].charAt(0))
+			{
+			case 'S':
+				tmp.addString(component[1], component[2]);
+				break;
+			case 'I':
+				tmp.addInt(component[1], Integer.parseInt(component[2]));
+				break;
+			case 'D':
+				tmp.addDouble(component[1], Double.parseDouble(component[2]));
+				break;
+			case 'B':
+				tmp.addBoolean(component[1], Boolean.parseBoolean(component[2]));
+				break;
+			}
+		}
+		
+		attributes.put(tmp.getName(), tmp);
+		
+	}
+	
+	/*
+	 * Methods for using/manipulating the BufferedReader
+	 */
+	
+	/**
+	 * When the config file is done being used, call this method to close the file.
+	 * @return Whether or not the file successfully closed
+	 */
+	public boolean done()
+	{
+		try
+		{
+			br.close();
+			return true;
+		}
+		catch(IOException ioe)
+		{
+			//TODO change the error output to run on a boolean value to change between output types
+			
+			//Use for stack trace debugging
+			//ioe.printStackTrace();
+			
+			//Use for user-friendly output
+			System.out.println("Error: Could not close config file \"" + getFilename() + "\"");
+			
+			return false;
+		}
+	}
+	
+	private String getText()
+	{
+		String str = "";
+		String line;
+		
+		try
+		{
+			line = br.readLine();
+			while(line != null)
+			{
+				if(line.trim().length() > 0 && line.trim().charAt(0) != '#')
+				{
+					str+=line + "\n";
+				}
+				line = br.readLine();
+			}
+			
+			return str;
+		}
+		catch(IOException ioe)
+		{
+			//TODO change the error output to run on a boolean value to change between output types
+			
+			//Use for stack trace debugging
+			//ioe.printStackTrace();
+			
+			//Use for user-friendly output
+			System.out.println("Error: Could not read the config file \"" + getFilename() + "\"");
+			return null;
+		}
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "Config file \"" + getFilename() + "\" has the following attributes:\n"
+				+ attributes;
+	}
+	
+	
+}
---
+++ ConfigAttribute.java
@@ -0,0 +1,160 @@
+package resources;
+
+import java.util.Hashtable;
+
+/**
+ * This class is the backbone for config.class. Each entry in a .conf file is a separate attribute.
+ * @author Hunter Herbst
+ *
+ */
+public class ConfigAttribute 
+{
+	
+	private String name;
+	private Hashtable<String, Integer> ints;
+	private Hashtable<String, Double> doubles;
+	private Hashtable<String, Boolean> booleans;
+	private Hashtable<String, String> strings;
+	
+	/**
+	 * Create a new configuration attribute.
+	 * @param name The name of the new attribute.
+	 */
+	public ConfigAttribute(String name)
+	{
+		setName(name);
+		ints = new Hashtable<String, Integer>();
+		doubles = new Hashtable<String, Double>();
+		booleans = new Hashtable<String, Boolean>();
+		strings = new Hashtable<String, String>();
+	}
+	
+	/**
+	 * Set the name of the configuration attribute.
+	 * @param name The new name of the attribute.
+	 */
+	private void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	/**
+	 * Get the name of the configuration attribute.
+	 * @return The configuration attribute's name.
+	 */
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	
+	/*
+	 * Methods for adding values to the attribute
+	 */
+	
+	/**
+	 * Attach an integer value to the attribute.
+	 * @param key The name of the integer to be attached
+	 * @param value The value of the integer
+	 */
+	public void addInt(String key, int value)
+	{
+		ints.put(key, value);
+	}
+	
+	/**
+	 * Attach a double value to the attribute.
+	 * @param key The name of the double to be attached
+	 * @param value The value of the double
+	 */
+	public void addDouble(String key, double value)
+	{
+		doubles.put(key, value);
+	}
+	
+	/**
+	 * Attach a boolean value to the attribute.
+	 * @param key The name of the boolean to be attached
+	 * @param value The value of the boolean
+	 */
+	public void addBoolean(String key, boolean value)
+	{
+		booleans.put(key, value);
+	}
+	
+	/**
+	 * Attach a String to the attribute.
+	 * @param key The name of the String to be attached
+	 * @param value The value of the String
+	 */
+	public void addString(String key, String value)
+	{
+		strings.put(key, value);
+	}
+	
+	
+	/*
+	 * Methods for getting values from the attribute
+	 */
+	
+	/**
+	 * Get an attached integer value from the attribute.
+	 * @param key The name of the attached integer to be retrieved
+	 * @return The value of the attached integer
+	 */
+	public int getInt(String key)
+	{
+		return ints.get(key);
+	}
+	
+	/**
+	 * Get an attached double value from the attribute.
+	 * @param key The name of the attached double to be retrieved
+	 * @return The value of the attached double
+	 */
+	public double getDouble(String key)
+	{
+		return doubles.get(key);
+	}
+	
+	/**
+	 * Get an attached boolean value from the attribute.
+	 * @param key The name of the attached boolean to be retrieved
+	 * @return The value of the attached boolean
+	 */
+	public boolean getBoolean(String key)
+	{
+		return booleans.get(key);
+	}
+	
+	/**
+	 * Get an attached String from the attribute.
+	 * @param key The name of the attached String to be retrieved
+	 * @return The value of the attached String
+	 */
+	public String getString(String key)
+	{
+		return strings.get(key);
+	}
+	
+	/*
+	 * Other Methods
+	 */
+	
+	
+	@Override
+	/**
+	 * Get the configuration attribute as a printable String
+	 */
+	public String toString()
+	{
+		String tmp = "";
+		tmp+=getName() + ":\n";
+		tmp+="\tStrings: " + strings + "\n";
+		tmp+="\tIntegers: " + ints + "\n";
+		tmp+="\tDoubles: " + doubles + "\n";
+		tmp+="\tBooleans: " + booleans;
+		
+		return tmp;
+	}
+}
